10ed893992b8bb0ff2440a5aaa09c23ed9b91c43
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2022-02-21 14:28:50 +0900

    feat: project setup

---
+++ src/main/java/xyz/cryptohows/backend/CryptoHowsApplication.java
@@ -0,0 +1,13 @@
+package xyz.cryptohows.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoHowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoHowsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/cryptohows/backend/CryptoHowsApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.cryptohows.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoHowsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
